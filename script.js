8bd391bd5d1d0583a9c28e67f937bb3f62fc6f2d
lewislemay <lalemay@ualberta.ca>
2025-03-04 15:50:45 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+numTotal = await input("Please input number: ");
+
+if (numTotal > 0) {
+
+numTotal = Number(numTotal);
+numDivide = await input("Please input divided number: ");
+numDivide = Number(numDivide);
+numGroups = numTotal / numDivide;
+output(numGroups);
+numRemain = numTotal % numDivide;
+output(numRemain);
+}
+else {
+output("Invalid input. Please try again.");
